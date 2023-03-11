ce0dab9e436de70d1e832cf4fe3e9bc56629d4c0
bhargavp2004 <105142165+bhargavp2004@users.noreply.github.com>
2023-03-12 03:47:46 +0530

    commit

--- HMS_APP/models.py
+++ HMS_APP/models.py
@@ -22,7 +22,7 @@ class Room(models.Model) :
 
     def __str__(self) :
         return f'Room number {self.number} is having capacity of {self.capacity} people and category is {self.category}'
-    
+
 class Booking(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
@@ -31,3 +31,5 @@ class Booking(models.Model):
 
     def __str__(self) :
         return f'{self.user} has booked room with {self.room} from {self.check_in} to {self.check_out}'
+
+        #changed through atom
